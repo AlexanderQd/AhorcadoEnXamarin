42ec2b136830c4743d7eb9a7bab30cc773a2ee82
AlexanderQd <manualex2193@gmail.com>
2017-09-25 18:21:42 +0100

    PrimeraApp 25-09-2017

--- Ahorcado/Ahorcado/MainPage.xaml.cs
+++ Ahorcado/Ahorcado/MainPage.xaml.cs
@@ -7,11 +7,26 @@ using Xamarin.Forms;
 
 namespace Ahorcado
 {
+    
     public partial class MainPage : ContentPage
     {
+        readonly string[] ALFABETO = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "Ñ", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
         public MainPage()
         {
             InitializeComponent();
+            Inicializar();
+        }
+
+        private void Inicializar()
+        {
+            imageAhorcado.Source = ImageSource.FromResource("Ahorcado.imagenes.1.jpg");
+            int fila = 0;
+            for (int c = 0; c < 7; c++)                
+                gridBotonera.Children.Add(new Button
+                {
+                    Text = ALFABETO[c],
+                    VerticalOptions = LayoutOptions.Center
+                }, c, fila);
         }
     }
 }
